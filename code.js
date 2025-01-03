eb9d57b138eb9ba6998c9032b8cb060234d372a6
JuSeRDev <juserdev@gmail.com>
2025-01-03 17:26:16 -0500

    Actualizacion de Landing Page con mediaQuery

--- code.js
+++ code.js
@@ -20,7 +20,7 @@ var home;
         d1.innerHTML = `${data[0].description}`;
         d2.innerHTML = `${data[1].description}`;
         f1.innerText = `${data[0].filter}`;
-        f2.innerText = `${data[2].filter}`;
+        f2.innerText = `${data[1].filter}`;
         url1.href = `/retos-js-ts/${data[0].url}`;
         url2.href = `/retos-js-ts/${data[1].url}`;
     })
