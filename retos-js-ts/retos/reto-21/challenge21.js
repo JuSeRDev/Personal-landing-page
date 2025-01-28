6d6cd5ce7918c2ef5eac3f3c553f522d4ee3a294
JuSeRDev <juserdev@gmail.com>
2025-01-28 16:35:56 -0500

    continuacion de codigo -> botones funcionando correctamente

--- retos-js-ts/retos/reto-21/challenge21.js
+++ retos-js-ts/retos/reto-21/challenge21.js
@@ -4,29 +4,51 @@ var backgroundDark = document.querySelector(".background-dark");
 var externals = document.querySelectorAll(".external");
 var internals = document.querySelectorAll(".internal");
 var footerSfs = document.querySelectorAll(".footer-sf");
-selects.forEach(function (select) {
-    select.addEventListener("click", function () {
-        sectionFour.style.display = "inline";
-        backgroundDark.style.display = "inline";
-    });
-});
+var closeMenu = document.querySelector(".icon-close-menu");
+var buttonProject = document.querySelector(".button1-so");
+var input = document.querySelectorAll(".input");
 var internalActive = "internal-active";
 var footerShow = "footer-sf-show";
+var toggle = function (param) {
+    sectionFour.style.display = "".concat(param);
+    backgroundDark.style.display = "".concat(param);
+};
+var toggleSelect = function (i) {
+    internals[i].classList.toggle(internalActive);
+    if (i === 1) {
+        footerSfs[0].classList.toggle("footer-sf-show");
+    }
+    else if (i === 2) {
+        footerSfs[1].classList.toggle("footer-sf-show");
+    }
+};
 var removeArrays = function (param, classes) {
     param.forEach(function (element) { return element.classList.remove(classes); });
 };
-externals.forEach(function (external, i) {
-    externals[i].addEventListener("click", function () {
-        removeArrays(internals, internalActive);
-        removeArrays(footerSfs, footerShow);
-        internals[i].classList.toggle(internalActive);
-        if (internals[1].classList.contains(internalActive)) {
-            removeArrays(footerSfs, footerShow);
+buttonProject.addEventListener("click", function () { return toggle("inline"); });
+selects.forEach(function (select, i) {
+    select.addEventListener("click", function () {
+        toggle("inline");
+        if (i === 0) {
+            internals[1].classList.toggle(internalActive);
             footerSfs[0].classList.toggle("footer-sf-show");
         }
-        else if (internals[2].classList.contains(internalActive)) {
-            removeArrays(footerSfs, footerShow);
+        else if (i === 1) {
+            internals[2].classList.toggle(internalActive);
             footerSfs[1].classList.toggle("footer-sf-show");
         }
     });
 });
+externals.forEach(function (external, i) {
+    external.addEventListener("click", function () {
+        removeArrays(internals, internalActive);
+        removeArrays(footerSfs, footerShow);
+        toggleSelect(i);
+    });
+});
+closeMenu.addEventListener("click", function () {
+    removeArrays(internals, internalActive);
+    removeArrays(footerSfs, footerShow);
+    toggle("none");
+});
+var cash = document.querySelector(".cash-inicial");
--- retos-js-ts/retos/reto-21/challenge21.ts
+++ retos-js-ts/retos/reto-21/challenge21.ts
@@ -4,35 +4,60 @@ const backgroundDark = document.querySelector(".background-dark") as HTMLDivElem
 const externals = document.querySelectorAll(".external") as NodeListOf<HTMLDivElement>
 const internals = document.querySelectorAll(".internal") as NodeListOf<HTMLDivElement>
 const footerSfs = document.querySelectorAll(".footer-sf") as NodeListOf<HTMLDivElement>
-
-
-selects.forEach(select => {
-    select.addEventListener("click", ()=>{
-        sectionFour.style.display = "inline"
-        backgroundDark.style.display = "inline"
-    })
-});
+const closeMenu = document.querySelector(".icon-close-menu") as SVGSVGElement
+const buttonProject = document.querySelector(".button1-so") as HTMLParagraphElement
+const input = document.querySelectorAll(".input") as NodeListOf<HTMLInputElement>
 
 const internalActive = "internal-active"
 const footerShow = "footer-sf-show"
+
+const toggle = (param: string) =>{
+    sectionFour.style.display = `${param}`
+    backgroundDark.style.display = `${param}`
+}
+
+const toggleSelect = (i: number)=>{
+    internals[i].classList.toggle(internalActive)
+    if (i === 1) {
+        footerSfs[0].classList.toggle("footer-sf-show")
+    } else if (i === 2) {
+        footerSfs[1].classList.toggle("footer-sf-show")
+    }
+}
 
 const removeArrays = (param: NodeListOf<HTMLDivElement>, classes: string) =>{
     param.forEach(element => element.classList.remove(classes));
 }
 
-externals.forEach((external, i) => {
-    externals[i].addEventListener("click", ()=>{
+buttonProject.addEventListener("click", ()=> toggle("inline") )
 
-        removeArrays(internals, internalActive)
-        removeArrays(footerSfs, footerShow)
+selects.forEach((select, i) => {
+    select.addEventListener("click", ()=> {
+        toggle("inline")
 
-        internals[i].classList.toggle(internalActive)
-        if (internals[1].classList.contains(internalActive)) {
-            removeArrays(footerSfs, footerShow)
+        if (i === 0) {
+            internals[1].classList.toggle(internalActive)
             footerSfs[0].classList.toggle("footer-sf-show")
-        } else if (internals[2].classList.contains(internalActive)) {
-            removeArrays(footerSfs, footerShow)
+        } else if (i === 1){
+            internals[2].classList.toggle(internalActive)
             footerSfs[1].classList.toggle("footer-sf-show")
         }
     })
 });
+
+externals.forEach((external, i) => {
+    external.addEventListener("click", ()=>{
+        removeArrays(internals, internalActive)
+        removeArrays(footerSfs, footerShow)
+        toggleSelect(i)
+    })
+});
+
+
+closeMenu.addEventListener("click", ()=>{
+    removeArrays(internals, internalActive)
+    removeArrays(footerSfs, footerShow)
+    toggle("none")
+})
+
+const cash = document.querySelector(".cash-inicial") as HTMLSpanElement
